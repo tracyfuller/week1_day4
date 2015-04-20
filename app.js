a5be0edf21ac6cc44ad3748923dd8c12f460d3e5
tracyfuller <tracy.e.fuller@gmail.com>
2015-04-20 00:06:51 -0500

    Updating JS and index.html files

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-console.log("Testing the script");
+console.log(document.write(document.getElementById("testing").value));
+
 var monster = {
-	name: ,
-	strength: ,
-	dexterity: ,
-	constitution: ,
-	wisdom: ,
-	intelligence: ,
-	charisma: 
+	name: document.write(document.getElementById("name").value),
+	strength: document.write(document.getElementById("strength").value),
+	dexterity: document.write(document.getElementById("dexterity").value,
+	constitution: document.write(document.getElementById("constitution").value),
+	wisdom: document.write(document.getElementById("wisdom").value),
+	intelligence: document.write(document.getElementById("intelligence").value),
+	charisma: document.write(document.getElementById("charisma").value)
 };
 
 var player = {
-	//inherits from monster? What does that mean?
-	//I have a funny feeling that having played D&D would have helped in this one
-	playerClass: 
+	name: monster.name,
+	strength: monster.strength,
+	dexterity: monster.dexterity,
+	constitution: monster.constitution,
+	wisdom: monster.wisdom,
+	intelligence: monster.intelligence,
+	charisma: monster.charisma,
+	playerClass: document.write(document.getElementById("class").value)
 };
 
 var storageArray = [];
@@ -24,3 +30,17 @@ var constitutionInput = document.write(document.getElementById("constitution").v
 var wisdomInput = document.write(document.getElementById("wisdom").value);
 var intelligenceInput = document.write(document.getElementById("intelligence").value);
 var charismaInput = document.write(document.getElementById("charisma").value);
+
+console.log(nameInput);
+
+if (document.write(document.getElementById("class").value) !== "") {
+	//put the values in as a player
+} else {
+	//put the values in as a monster
+}
+
+function createMonster(name, strength, dexterity, constitution, wisdom, intelligence, charisma){
+
+}
+
+document.getElementById("addMonster").addEventListener('click', <functionNameGoesHere>);
